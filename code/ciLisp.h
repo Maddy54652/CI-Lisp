dce9c5b8d78b5890e96e049d1826d104ea24c81e
Maddy <37761862+Maddy54652@users.noreply.github.com>
2019-04-10 14:20:45 -0700

    Update ciLisp.h

--- code/ciLisp.h
+++ code/ciLisp.h
@@ -17,37 +17,34 @@ int yylex(void);
 void yyerror(char *);
 
 typedef enum oper { // must be in sync with funcs in resolveFunc()
-    NEG,
-    ABS,
-    EXP,
-    SQRT,
-    ADD,
-    SUB,
-    MULT,
-    DIV,
-    REMAINDER,
-    LOG,
-    POW,
-    MAX,
-    MIN,
-    EXP2,
-    CBRT,
-    HYPOT,
-    INVALID_OPER=255
+    NEG_OPER, // 0
+    ABS_OPER, // 1
+    EXP_OPER, // 2
+    SQRT_OPER, // 3
+    ADD_OPER, // 4
+    SUB_OPER, // 5
+    MULT_OPER, // 6
+    DIV_OPER, // 7
+    REMAINDER_OPER, // 8
+    LOG_OPER, // 9
+    POW_OPER, // 10
+    MAX_OPER, // 11
+    MIN_OPER, // 12
+    EXP2_OPER, // 13
+    CBRT_OPER, // 14
+    HYPOT_OPER, // 15
+    CUSTOM_FUNC=255
 } OPER_TYPE;
 
+OPER_TYPE resolveFunc(char *);
+
 typedef enum {
-    NUM_TYPE, FUNC_TYPE,SYMBOL_TYPE,LET_LIST_TYPE,LET_ELEM_TYPE,SCOPE_TYPE
+    NUM_TYPE, FUNC_TYPE,SYMB_TYPE
 } AST_NODE_TYPE;
 
 typedef struct {
     double value;
 } NUMBER_AST_NODE;
-
-typedef struct{
-    char* name;
-    double value;
-}SYMBOL_AST_NODE;
 
 typedef struct {
     char *name;
@@ -55,51 +52,26 @@ typedef struct {
     struct ast_node *op2;
 } FUNCTION_AST_NODE;
 
-typedef struct{
-    struct ast_node *let_elem;
-    struct ast_node *let_list;
-}LET_LIST_AST_NODE;
-
-typedef struct{
-    struct ast_node *symbol;
-    struct ast_node *sExpr;
-}LET_ELEMENT_AST_NODE;
+typedef struct symbol_ast_node{
+    char* name;
+}SYMBOL_AST_NODE;
 
 typedef struct symbol_table_node{
     char* ident;
     struct ast_node *val;
     struct symbol_table_node *next;
-    struct symbol_table_node *prev;
 }SYMBOL_TABLE_NODE;
-
-typedef struct symbol_table{
-    SYMBOL_TABLE_NODE* head;
-    SYMBOL_TABLE_NODE* tail;
-}SYMBOL_TABLE;
-
-typedef struct scope{
-    struct ast_node* theList;
-    struct ast_node* sExpr;
-}SCOPE_NODE;
 
 typedef struct ast_node {
     AST_NODE_TYPE type;
+    SYMBOL_TABLE_NODE *symbolTable;
+    struct ast_node *parent;
     union {
         NUMBER_AST_NODE number;
         FUNCTION_AST_NODE function;
         SYMBOL_AST_NODE symbol;
-        LET_LIST_AST_NODE list;
-        LET_ELEMENT_AST_NODE element;
-        SCOPE_NODE theScope;
     } data;
 } AST_NODE;
-
-
-//the symbol table is used to keep track of symbols and values
-SYMBOL_TABLE* symbolTable;
-
-//currentNode keeps the spot in the symbol table
-SYMBOL_TABLE_NODE* currentNode;
 
 AST_NODE *number(double value);
 
@@ -109,11 +81,16 @@ void freeNode(AST_NODE *p);
 
 double eval(AST_NODE *ast);
 
-AST_NODE* scope(AST_NODE* letList,AST_NODE* sExpres);
+AST_NODE *setSymbolTable(SYMBOL_TABLE_NODE *symbolTable, AST_NODE *s_expr);
 
-AST_NODE* symbol(char* theName,double theValue);
+AST_NODE *symbol (char* name);
 
-LET_LIST_AST_NODE* let_list(LET_LIST_AST_NODE* theList,LET_ELEMENT_AST_NODE* theElement);
+SYMBOL_TABLE_NODE *createSymbol(char *name, AST_NODE *value);
 
-LET_ELEMENT_AST_NODE* let_element(SYMBOL_AST_NODE* input, AST_NODE* sExpr);
+SYMBOL_TABLE_NODE* findSymbol(SYMBOL_TABLE_NODE *symbolTable, SYMBOL_TABLE_NODE *symbol);
+
+SYMBOL_TABLE_NODE *addSymbolToList(SYMBOL_TABLE_NODE *symbolTable, SYMBOL_TABLE_NODE *newSymbol);
+
+SYMBOL_TABLE_NODE* resolveSymbol(char* name,AST_NODE* node);
+
 #endif
