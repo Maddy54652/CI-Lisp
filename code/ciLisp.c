8fc46894f1129e64a51810354066012ce636218b
Maddy <37761862+Maddy54652@users.noreply.github.com>
2019-05-02 22:55:02 -0700

    not finished yet

--- code/ciLisp.c
+++ code/ciLisp.c
@@ -95,68 +95,44 @@ void freeNode(AST_NODE *p) {
 //
 // p points to the root
 //
-RETURN_TYPE eval(AST_NODE *p) {
-    if (!p)
-        return 0.0;
-    SYMBOL_TABLE_NODE *temp;
-// TBD: implement
-
-    switch (p->type) {
-        case NUM_TYPE:
-            return p->data.number.value;
-        case FUNC_TYPE:
-            switch (resolveFunc(p->data.function.name)) {
-                case NEG_OPER:
-                    return ((-1) * eval(p->data.function.op1));
-                case ABS_OPER:
-                    return fabs(eval(p->data.function.op1));
-                case EXP_OPER:
-                    return exp(eval(p->data.function.op1));
-                case SQRT_OPER:
-                    return sqrt(eval(p->data.function.op1));
-                case ADD_OPER:
-                    return ((eval(p->data.function.op1)) + (eval(p->data.function.op2)));
-                case SUB_OPER:
-                    return ((eval(p->data.function.op1)) - (eval(p->data.function.op2)));
-                case MULT_OPER:
-                    return ((eval(p->data.function.op1)) * (eval(p->data.function.op2)));
-                case DIV_OPER:
-                    return ((eval(p->data.function.op1)) / (eval(p->data.function.op2)));
-                case REMAINDER_OPER:
-                    return remainder((eval(p->data.function.op1)), (eval(p->data.function.op2)));
-                case LOG_OPER:
-                    return (log(eval(p->data.function.op1)));
-                case POW_OPER:
-                    return (pow((eval(p->data.function.op1)), (eval(p->data.function.op2))));
-                case MAX_OPER:
-                    if ((eval(p->data.function.op1)) > (eval(p->data.function.op2))) {
-                        return eval(p->data.function.op1);
-                    } else {
-                        return eval(p->data.function.op2);
-                    }
-                case MIN_OPER:
-                    if ((eval(p->data.function.op1)) < (eval(p->data.function.op2))) {
-                        return eval(p->data.function.op1);
-                    } else {
-                        return eval(p->data.function.op2);
-                    }
-                case EXP2_OPER:
-                    return exp2((eval(p->data.function.op1)));
-                case CBRT_OPER:
-                    return (cbrt(eval(p->data.function.op1)));
-                case HYPOT_OPER:
-                    return (hypot((eval(p->data.function.op1)), (eval(p->data.function.op2))));
-            }
-        case SYMB_TYPE:
-
-            temp = resolveSymbol(p->data.symbol.name, p);
-            if (temp == NULL) {
-                puts("there is an error");
-                exit(1);
-            }
-            return eval(temp->val);
-    }
-    return 0.0;
+RETURN_VALUE eval(AST_NODE *p) { 
+     RETURN_VALUE temp;
+     if (!p){
+         temp->type = NO_TYPE;
+         temp->value = 0.0;
+         return temp;
+     }
+  
+   switch(p->type){
+       case INTEGER_TYPE:
+           //evaluate number
+           AST_NODE temp2;
+           temp2 = number(p->data.number->value,INTEGER_TYPE);
+           temp->value = temp2->data.number->value;
+           temp->type = INTEGER_TYPE;
+           return temp;
+           
+       case REAL_TYPE:
+           //evaluate number
+           AST_NODE temp2;
+           temp2 = number(p->data.number->value,REAL_TYPE);
+           temp->value = temp2->data.number->value;
+           temp->type = REAL_TYPE;
+           return temp;
+           
+       case FUNCTION_TYPE:
+           //evaluate function
+           return temp;
+           
+       case SYMBOL_TYPE:
+           //evaluate symbol
+           return temp;
+           
+       default:
+           temp->type = NO_TYPE;
+           temp->value = 0.0;
+           break;
+   }
 }
 
 AST_NODE *setSymbolTable(SYMBOL_TABLE_NODE *symbolTable, AST_NODE *s_expr) {
